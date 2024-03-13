b5e5731951bc1418aef3e163680ea3da45048f82
Md.Sanjidul Hasan Hasib <sanjidhasib39@gmail.com>
2024-03-13 17:11:42 +0600

    added some features

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header1 from './Components/Header/Header1'
 import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/bookmarks/Bookmarks'
 import './App.css'
 
 function App() {
@@ -7,7 +8,10 @@ function App() {
   return (
     <>
     <Header1></Header1>
-    <Blogs></Blogs>
+   <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+   </div>
     </>
   )
 }
--- src/Components/Header/Header1.jsx
+++ src/Components/Header/Header1.jsx
@@ -1,9 +1,10 @@
-
+import profile from '../../assets/images/profile.png'
 const Header1 = () => {
     return (
-        <div>
-            <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
-        </div>
+        <header className="flex justify-between items-center mx-5 p-4 border-b-2">
+            <h1 className="text-4xl font-bold  ">Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+        </header>
     );
 };
 
